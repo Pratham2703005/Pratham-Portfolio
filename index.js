5de0048938b05e35bd1317f2f77a2c32c8ce88e5
Pratham Israni <155840595+Pratham2703005@users.noreply.github.com>
2024-06-29 20:54:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ document.querySelector('#dark-mode-toggle').addEventListener('click', () => {
     document.querySelector('meta[name="theme-color"').setAttribute('content', isDarkMode ? '#1a1a2e' : '#fff');
 });
 
-document.getElementById('hamburger').addEventListener('click', function () {
+document.getElementById('hamburger').addEventListener('click', function (event) {
+    event.stopPropagation();
     document.getElementById('nav-item').classList.toggle('active');
+});
+
+document.querySelector('#wrapper').addEventListener('click', () => {
+    const navItem = document.getElementById('nav-item');
+    if (navItem.classList.contains('active')) {
+        navItem.classList.remove('active');
+    }
 });
